eb56092e53684b621cfb77f2256b98cb64d721b4
amjd <amjad.4@gmail.com>
2014-04-16 20:17:04 +0530

    Update API to Bitstamp

--- btc.py
+++ btc.py
@@ -1,4 +1,4 @@
 import urllib2, json
-x = float(json.loads(urllib2.urlopen('https://mtgox.com/api/1/BTCUSD/ticker').read())['return']['last']['value'])
-print x
+rate = float(json.loads(urllib2.urlopen('https://www.bitstamp.net/api/ticker/').read())['last'])
+print rate
 
